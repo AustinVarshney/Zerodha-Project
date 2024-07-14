5372ad3a795ad62379d10528b2f17745c907881a
Austin Varshney <austinvarshney@gmail.com>
2024-07-14 11:13:52 +0530

    Problem Solved

--- dashboard/src/components/Summary.jsx
+++ dashboard/src/components/Summary.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import "./Summary.css"
 
 const Summary = () => {
   return (
-    <div>
-      <p>Some text is there in Summary</p>
+    <div className='outerSummaryDiv'>
+      <div className='innerSummaryDiv1'>
+        <p>Hi, User!</p>
+      </div>
+      <div className='innerSummaryDiv2'>
+        <div className='innerMostSummaryDiv1'>
+          <p>Equity</p>
+        </div>
+        <div className='innerMostSummaryDiv2'>
+          <div>
+            <p className='innerParaSummaryDiv1'>3.74k</p>
+            <p className='innerParaSummaryDiv2'>Margin available</p>
+          </div>
+          <div>
+            <p className='innerParaSummaryDiv3'>Margins used&nbsp; <span>0</span></p>
+            <p className='innerParaSummaryDiv4'>Opening balance&nbsp; <span>3.74k</span></p>
+          </div>
+        </div>
+      </div>
+      <div className='innerSummaryDiv3'>
+        <div className='innerMostSummaryDiv3'>
+          <p>Holdings (13)</p>
+        </div>
+        <div className='innerMostSummaryDiv4'>
+          <div>
+            <p className='innerParaSummaryDiv5'>1.55k <span>+5.20%</span></p>
+            <p className='innerParaSummaryDiv6'>P&L</p>
+          </div>
+          <div>
+            <p className='innerParaSummaryDiv7'>Current Value&nbsp; <span>31.43k</span></p>
+            <p className='innerParaSummaryDiv8'>Investment&nbsp; <span>29.88k</span></p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- dashboard/src/components/Watchlist.jsx
+++ dashboard/src/components/Watchlist.jsx
@@ -1,62 +1,65 @@
-import React, { useState } from 'react'
-import "./Watchlist.css"
-import SearchIcon from '@mui/icons-material/Search';
-import { watchlist } from '../assets/assets';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import { Tooltip } from 'react-tooltip';
+import React, { useState } from "react";
+import "./Watchlist.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { watchlist } from "../assets/assets";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Tooltip } from "react-tooltip";
 
 const Watchlist = () => {
-  let [isHovering, setIsHovering] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
-  const handleMouseOver = (id, index) => {
-    setIsHovering(() => {
-      if(watchlist[index].id === id){
-        return (true);
-      } else{
-        return (false);
-      }
-    })
+  const handleMouseOver = (index) => {
+    setHoveredIndex(index);
   };
 
   const handleMouseOut = () => {
-    setIsHovering(false);
+    setHoveredIndex(null);
   };
 
   return (
-    <div className='outerWatchlistDiv1'>
-      <div className='innerWatchlistDiv1'>
-        <SearchIcon style={{ color: "#616161" }} /><input type="text" placeholder='Search eg: infy, bse, nifty fut, nifty weekly, gold mcx' />
+    <div className="outerWatchlistDiv1">
+      <div className="innerWatchlistDiv1">
+        <SearchIcon style={{ color: "#616161" }} />
+        <input type="text" placeholder="Search eg: infy, bse, nifty fut, nifty weekly, gold mcx"/>
         <p>9/20</p>
       </div>
-      <div className='outerWatchlistDiv2'>
-        {watchlist.map((item,index) => {
-          return (
-            <div key={item.id} className='innerWatchlistDiv2' onMouseOver={() => handleMouseOver(item.id, index)} onMouseOut={handleMouseOut}>
-              <div className='innerMostWatchlistDiv1'>
-                <p style={item.isDown ? { color: "#DF4949" } : { color: "#67C988" }}>{item.name}</p>
-              </div>
-              <div className='innerMostWatchlistDiv2'>
-                <p className='innerMostWatchlistPara1'>{item.percent}</p>
-                <p className='innerMostWatchlistPara2' style={item.isDown ? {color: "#DF4949"} : {color: "#67C988"}}>{item.isDown ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}</p>
-                <p className='innerMostWatchlistPara3' style={item.isDown ? {color: "#DF4949"} : {color: "#67C988"}}>{item.price}</p>
-              </div>
-              <div className='innerMostWatchlistDiv3' style={isHovering ? {} : {display: "none"}}>
-                <button style={{backgroundColor: "#0070ff", color: "white"}}><a data-tooltip-id="my-tooltip-1" data-tooltip-content="Buy (B)">Buy</a></button>
-                <button style={{backgroundColor: "#ff4600", color: "white"}}><a data-tooltip-id="my-tooltip-1" data-tooltip-content="Sell (S)">Sell</a></button>
-                <button style={{backgroundColor: "white", color: "gray", border: "0.5px solid gray"}}><a data-tooltip-id="my-tooltip-2" data-tooltip-content="Analytics (A)"><BarChartIcon style={{display: "flex"}}/></a></button>
-                <button style={{backgroundColor: "white", color: "gray", border: "0.5px solid gray"}}><a data-tooltip-id="my-tooltip-1" data-tooltip-content="More"><MoreHorizIcon style={{display: "flex"}}/></a></button>
-                <Tooltip id="my-tooltip-1" style={{height: "0.25rem", display: "flex", alignItems: "center", width: "2.2rem", fontSize: "0.7rem", backgroundColor: "gray", textAlign: "center"}}/>
-                <Tooltip id='my-tooltip-2' style={{height: "0.25rem", display: "flex", alignItems: "center", width: "4rem", fontSize: "0.7rem", backgroundColor: "gray", textAlign: "center"}}/> 
-              </div>
+      <div className="outerWatchlistDiv2">
+        {watchlist.map((item, index) => (
+          <div key={item.id} className="innerWatchlistDiv2" onMouseOver={() => handleMouseOver(index)} onMouseOut={handleMouseOut}>
+            <div className="innerMostWatchlistDiv1">
+              <p style={item.isDown ? { color: "#DF4949" } : { color: "#67C988" }}>{item.name}</p>
             </div>
-          )
-        })}
+            <div className="innerMostWatchlistDiv2">
+              <p className="innerMostWatchlistPara1">{item.percent}</p>
+              <p className="innerMostWatchlistPara2" style={item.isDown ? { color: "#DF4949" } : { color: "#67C988" }}>{item.isDown ? (<KeyboardArrowDownIcon />) : (<KeyboardArrowUpIcon />)}</p>
+              <p className="innerMostWatchlistPara3" style={item.isDown ? { color: "#DF4949" } : { color: "#67C988" }}>{item.price}</p>
+            </div>
+            <div className="innerMostWatchlistDiv3" style={hoveredIndex === index ? {} : { display: "none" }}>
+              <button style={{ backgroundColor: "#0070ff", color: "white" }}>
+                <a data-tooltip-id="my-tooltip-1" data-tooltip-content="Buy (B)">Buy</a>
+              </button>
+              <button style={{ backgroundColor: "#ff4600", color: "white" }}>
+                <a data-tooltip-id="my-tooltip-1" data-tooltip-content="Sell (S)">Sell</a>
+              </button>
+              <button style={{backgroundColor: "white",color: "gray",border: "0.5px solid gray",}}>
+                <a data-tooltip-id="my-tooltip-2" data-tooltip-content="Analytics (A)"><BarChartIcon style={{ display: "flex" }} /></a>
+              </button>
+              <button style={{backgroundColor: "white",color: "gray",border: "0.5px solid gray"}}>
+                <a data-tooltip-id="my-tooltip-1" data-tooltip-content="More">
+                  <MoreHorizIcon style={{ display: "flex" }} />
+                </a>
+              </button>
+              <Tooltip id="my-tooltip-1" style={{height: "0.25rem",display: "flex",alignItems: "center",width: "2.2rem",fontSize: "0.7rem",backgroundColor: "gray",textAlign: "center",}}/>
+              <Tooltip id="my-tooltip-2" style={{height: "0.25rem",display: "flex",alignItems: "center",width: "4rem",fontSize: "0.7rem",backgroundColor: "gray",textAlign: "center",}}/>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Watchlist
+export default Watchlist;
