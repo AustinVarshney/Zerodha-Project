ff5b96c51a7e937fe753ef9394e72f413eb55604
Austin Varshney <austinvarshney@gmail.com>
2024-07-17 12:26:34 +0530

    Problem of Active Navbar Menu is solved

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -1,14 +1,38 @@
 import React, { useState } from 'react'
 import "./Menu.css"
 import logoImg from "../../src/assets/logo.png"
+import { Link, useLocation } from "react-router-dom"
 
 const Menu = () => {
+  let location = useLocation();
   let [activeMenu, setActiveMenu] = useState(0);
 
-  const activeClass = (index) => {
-    setActiveMenu(index)
-    console.log(activeMenu)
-  }
+  // Update active state based on pathname
+  React.useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        setActiveMenu(0);
+        break;
+      case "/orders":
+        setActiveMenu(1);
+        break;
+      case "/holdings":
+        setActiveMenu(2);
+        break;
+      case "/positions":
+        setActiveMenu(3);
+        break;
+      case "/funds":
+        setActiveMenu(4);
+        break;
+      case "/apps":
+        setActiveMenu(5);
+        break;
+      default:
+        setActiveMenu(0); // Default to Dashboard
+        break;
+    }
+  }, [location.pathname]);
 
   const menu = "menu";
   const selectedMenu = "selected-menu";
@@ -16,16 +40,16 @@ const Menu = () => {
   return (
     <div className='outerMenuDiv'>
       <div className='innerMenuDiv1'>
-        <img src={logoImg} />
+        <img src={logoImg} alt="Logo"/>
       </div>
       <div className='innerMenuDiv2'>
         <ul>
-          <li><a href="/" className={activeMenu===0 ? selectedMenu : menu} onClick={() => activeClass(0)}>Dashboard</a></li>
-          <li><a href="/orders" className={activeMenu===1 ? selectedMenu : menu} onClick={() => activeClass(1)}>Orders</a></li>
-          <li><a href="/holdings" className={activeMenu===2 ? selectedMenu : menu} onClick={() => activeClass(2)}>Holdings</a></li>
-          <li><a href="/positions" className={activeMenu===3 ? selectedMenu : menu} onClick={() => activeClass(3)}>Positions</a></li>
-          <li><a href="/funds" className={activeMenu===4 ? selectedMenu : menu} onClick={() => activeClass(4)}>Funds</a></li>
-          <li><a href="/apps" className={activeMenu===5 ? selectedMenu : menu} onClick={() => activeClass(5)}>Apps</a></li>
+          <li><Link to="/" className={activeMenu===0 ? selectedMenu : menu}>Dashboard</Link></li>
+          <li><Link to="/orders" className={activeMenu===1 ? selectedMenu : menu}>Orders</Link></li>
+          <li><Link to="/holdings" className={activeMenu===2 ? selectedMenu : menu}>Holdings</Link></li>
+          <li><Link to="/positions" className={activeMenu===3 ? selectedMenu : menu}>Positions</Link></li>
+          <li><Link to="/funds" className={activeMenu===4 ? selectedMenu : menu}>Funds</Link></li>
+          <li><Link to="/apps" className={activeMenu===5 ? selectedMenu : menu}>Apps</Link></li>
           <li><a>
             <div>
               ZU
