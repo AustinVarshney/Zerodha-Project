3656fe1dc3954f28fc5423b2a964ef47e0edf4fd
Austin Varshney <austinvarshney@gmail.com>
2024-07-17 10:33:04 +0530

    Sticked on the problem of Active Navbar

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Menu.css"
 import logoImg from "../../src/assets/logo.png"
 
 const Menu = () => {
+  let [activeMenu, setActiveMenu] = useState(0);
+
+  const activeClass = (index) => {
+    setActiveMenu(index)
+    console.log(activeMenu)
+  }
+
+  const menu = "menu";
+  const selectedMenu = "selected-menu";
+
   return (
     <div className='outerMenuDiv'>
       <div className='innerMenuDiv1'>
@@ -10,12 +20,12 @@ const Menu = () => {
       </div>
       <div className='innerMenuDiv2'>
         <ul>
-          <li><a href="/">Dashboard</a></li>
-          <li><a href="/orders">Orders</a></li>
-          <li><a href="/holdings">Holdings</a></li>
-          <li><a href="/positions">Positions</a></li>
-          <li><a href="/funds">Funds</a></li>
-          <li><a href="/apps">Apps</a></li>
+          <li><a href="/" className={activeMenu===0 ? selectedMenu : menu} onClick={() => activeClass(0)}>Dashboard</a></li>
+          <li><a href="/orders" className={activeMenu===1 ? selectedMenu : menu} onClick={() => activeClass(1)}>Orders</a></li>
+          <li><a href="/holdings" className={activeMenu===2 ? selectedMenu : menu} onClick={() => activeClass(2)}>Holdings</a></li>
+          <li><a href="/positions" className={activeMenu===3 ? selectedMenu : menu} onClick={() => activeClass(3)}>Positions</a></li>
+          <li><a href="/funds" className={activeMenu===4 ? selectedMenu : menu} onClick={() => activeClass(4)}>Funds</a></li>
+          <li><a href="/apps" className={activeMenu===5 ? selectedMenu : menu} onClick={() => activeClass(5)}>Apps</a></li>
           <li><a>
             <div>
               ZU
