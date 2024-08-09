46dcae79cceb75f79f9883a47128eea475cdeebe
Austin Varshney <austinvarshney@gmail.com>
2024-08-09 15:56:11 +0530

    Done with Few changes in dashboard

--- dashboard/vite.config.cjs
+++ dashboard/vite.config.cjs
@@ -4,5 +4,11 @@ const react = require('@vitejs/plugin-react');
 // https://vitejs.dev/config/
 module.exports = defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true // This is typically for dev mode but let's ensure it's set.
+  }
 });
 
